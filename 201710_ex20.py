bf4a450950fb99ce3f4fbaa70118194944a35fd4
lucsikora <lucsikora@users.noreply.github.com>
2017-10-29 21:48:42 +0100

    Exercise Done

--- 201710_ex20.py
+++ 201710_ex20.py
@@ -4,7 +4,10 @@
 # Date: 28th October, 2017
 #
 #
-# Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+# Write a function that takes an ordered list of numbers
+# (a list where the elements are in order from smallest to largest) and another number.
+# The function decides whether or not the given number is inside the list and returns (then prints)
+# an appropriate boolean.
 #
 # Extras:
 #
@@ -12,9 +15,15 @@
 
 
 def get_info():
-    user_list = input("Please write a list of numbers: ")
-    user_number = input("Plesae write a number your're looking for in a list: ")
-    user_list = [1, 2, 3, 4, 5, 6, 7, 8]
-    user_number = 5
-    return user_number user_list
+    user_list = list(input("Please write a list of numbers: "))
+    user_number = int(input("Plesae write a number your're looking for in a list: "))
+    #user_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    #user_number = 5
+    #print(user_list)
+    if user_number in user_list:
+        return True
+    else:
+        return False
 
+
+print(get_info())
