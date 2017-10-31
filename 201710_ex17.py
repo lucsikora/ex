4985caa48b09293fb514714d3ea3ebc66d4968ed
lucsikora <lucsikora@users.noreply.github.com>
2017-10-31 11:41:49 +0100

    Correction to Ex.17

--- 201710_ex17.py
+++ 201710_ex17.py
@@ -1,7 +1,7 @@
 # practicepython.py
 #
 # Solution by: Lukasz Sikora
-# Date: 30th October, 2017
+# Date: 31st October, 2017
 #
 #
 # Use the BeautifulSoup and requests Python packages to print out a list
@@ -14,8 +14,6 @@ url = 'https://www.nytimes.com/'
 r = requests.get(url)
 r_html = r.text
 
-soup = BeautifulSoup(r_html, "lxml")
-# title = soup.find_all("h2", {"class": "story-heading"})
+soup = BeautifulSoup(r_html, "html.parser")
 for div in soup.find_all("h2", {"class": "story-heading"}):
     print(div.find("a").string)
-print(title)
