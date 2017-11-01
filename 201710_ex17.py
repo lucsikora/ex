c93ef90502364e714ae7cc524edd69b14dbd8699
lucsikora <lucsikora@users.noreply.github.com>
2017-11-01 11:46:18 +0100

    Correction to Ex.17

--- 201710_ex17.py
+++ 201710_ex17.py
@@ -14,6 +14,15 @@ url = 'https://www.nytimes.com/'
 r = requests.get(url)
 r_html = r.text
 
-soup = BeautifulSoup(r_html, "html.parser")
-for div in soup.find_all("h2", {"class": "story-heading"}):
-    print(div.find("a").string)
+# soup = BeautifulSoup(r_html, "html.parser")
+# for div in soup.find_all("h2", {"class": "story-heading"}):
+#     print(div.find("a").string)
+
+soup = BeautifulSoup(r.text, "html.parser")
+headings = soup.findAll("h2", {"class": "story-heading"})
+
+for heading in headings:
+    if heading.findAll("a"):
+        print(heading.a.text.strip())
+    else:
+        print(heading.text)
