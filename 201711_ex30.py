040da7e66aed22acfb0575ec601abc9c6b6fc9f2
lucsikora <lucsikora@users.noreply.github.com>
2017-11-07 23:37:14 +0100

    Ex 30

---
+++ 201711_ex30.py
@@ -0,0 +1,14 @@
+# practicepython.py
+#
+# Solution by: Lukasz Sikora
+# Date: 7th November, 2017
+#
+#
+# This exercise is Part 1 of 3 of the Hangman exercise series. The other exercises are: Part 2 and Part 3.
+#
+# In this exercise, the task is to write a function that picks a random word from a list of words from
+# the SOWPODS dictionary. Download this file and save it in the same directory as your Python code. This file
+# is Peter Norvig’s compilation of the dictionary of words used in professional Scrabble tournaments. Each line in
+# the file contains a single word.
+#
+# Hint: use the Python random library for picking a random word.
