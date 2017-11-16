891eeab592225e0a4a457df9cc2eb16d328ae5b5
lucsikora <lucsikora@users.noreply.github.com>
2017-11-16 23:38:25 +0100

    Ex 22

--- 201711_ex33.py
+++ 201711_ex33.py
@@ -6,7 +6,10 @@
 #
 # This exercise is Part 1 of 4 of the birthday data exercise series. The other exercises are: Part 2, Part 3, and Part 4.
 #
-# For this exercise, we will keep track of when our friend’s birthdays are, and be able to find that information based on their name. Create a dictionary (in your file) of names and birthdays. When you run your program it should ask the user to enter a name, and return the birthday of that person back to them. The interaction should look something like this:
+# For this exercise, we will keep track of when our friend’s birthdays are, and be able to find that information
+# based on their name. Create a dictionary (in your file) of names and birthdays. When you run your program it should
+# ask the user to enter a name, and return the birthday of that person back to them. The interaction should look
+#  something like this:
 #
 # >>> Welcome to the birthday dictionary. We know the birthdays of:
 # Albert Einstein
