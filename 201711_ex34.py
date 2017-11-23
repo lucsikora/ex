c11e1b7abbd25439819ea5f91f252b2e1203dc44
lucsikora <lucsikora@users.noreply.github.com>
2017-11-23 23:16:47 +0100

    Ex 22

---
+++ 201711_ex34.py
@@ -0,0 +1,15 @@
+# practicepython.py
+#
+# Solution by: Lukasz Sikora
+# Date: 23rd November, 2017
+#
+#
+# This exercise is Part 2 of 4 of the birthday data exercise series. The other exercises are: Part 1, Part 3, and Part 4.
+#
+# In the previous exercise we created a dictionary of famous scientists’ birthdays. In this exercise, modify your
+# program from Part 1 to load the birthday dictionary from a JSON file on disk, rather than having the dictionary
+# defined in the program.
+#
+# Bonus: Ask the user for another scientist’s name and birthday to add to the dictionary, and update the JSON file you
+#  have on disk with the scientist’s name. If you run the program multiple times and keep adding new names, your JSON
+# file should keep getting bigger and bigger.
