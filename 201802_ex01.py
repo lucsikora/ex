2f10e3b2227d798664cc16abfaec1d439def3ec2
lucsikora <lucsikora@users.noreply.github.com>
2018-02-14 21:47:18 +0100

    Testing commit

---
+++ 201802_ex01.py
@@ -0,0 +1,7 @@
+# practicepython.py
+
+print('Wyswietlimy twoja wiadomosc N razy')
+usr_msg = input('Podaj wiadomosc: ')
+usr_num = input('Podaj ilosc N: ')
+
+print((usr_msg + "\n") * int(usr_num))
